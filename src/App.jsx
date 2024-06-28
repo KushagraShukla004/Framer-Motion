d7e962d219784ba70f5372d2e879133735c910f4
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-06-28 17:37:40 +0530

    Initial Structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import AnimationControls from "../components/Animationcontrols";
+import BasicsOfMotion from "../components/BasicsOfMotion";
+// import Gestures from "../components/Gestures";
+// import ScrollAnimations from "../components/ScrollAnimation";
+// import ViewBasedAnimations from "../components/ViewBaseAnimaions";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <BasicsOfMotion />
+      {/* <Gestures /> */}
+      {/* <AnimationControls /> */}
+      {/* <ViewBasedAnimations /> */}
+      {/* <ScrollAnimations /> */}
+    </div>
+  );
+}
---
+++ src/components/BasicsOfMotion.js
@@ -0,0 +1,15 @@
+const BasicsOfMotion = () => {
+  return (
+    <div
+      //this style is just to make everything in center
+      style={{
+        display: "grid",
+        placeContent: "center",
+        height: "100vh",
+        gap: "0.8rem",
+      }}
+    ></div>
+  );
+};
+
+export default BasicsOfMotion;
